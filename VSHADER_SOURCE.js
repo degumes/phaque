c11f591303fc03fa55001b8e6455d9b120970962
degumes <degumes@users.noreply.github.com>
2018-03-02 16:08:24 -0400

    perspective

--- VSHADER_SOURCE.js
+++ VSHADER_SOURCE.js
@@ -1,18 +1,23 @@
 const VSHADER_SOURCE = `#version 300 es
 in vec4 label;
+vec4 viewed;
 uniform mat4 aimSpin;
 uniform vec4 vertexCoordinates[8];
 out vec4 color;
 
 void main() {
   vec4 vertex = vertexCoordinates[int(label)];
+
   if (vertex.z < 0.0) {
     color = vec4(1.0, 0.8, 0.8, 1.0);
   }else{
     color = vec4(0.1, 0.1, 0.1, 1.0);
   }
 
-  gl_Position = aimSpin * vertex;
+  viewed = aimSpin * vertex;
+  gl_Position = vec4(viewed.xy / (0.25 * viewed.z + 1.0),viewed.zw);
 }`
 
 export default VSHADER_SOURCE
+
+// gl_Position = vec4(position.xy / zToDivideBy, position.zw)
--- vertexCoordinates.js
+++ vertexCoordinates.js
@@ -1,4 +1,4 @@
-const scl = 2.0 // scale factor
+const scl = 6.0 // scale factor
 
 const vertexCoordinates = new Float32Array([
   1.0, 1.0, 1.0, scl,
