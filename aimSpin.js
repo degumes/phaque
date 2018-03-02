bfdcc909113780d7ad751e15c7c01b5ff90464af
degumes <degumes@users.noreply.github.com>
2018-03-01 22:20:55 -0400

    spinThetaPhi --> aimSpin

--- aimSpin.js
+++ aimSpin.js
@@ -49,11 +49,11 @@ const aimSpin = function aimSpin ({theta, phi, spin}) {
   matrix[0] = matrixRotAU[0] * matrixRotZ[0] + matrixRotAU[1] * matrixRotZ[4]
   matrix[1] = matrixRotAU[4] * matrixRotZ[0] + matrixRotAU[5] * matrixRotZ[4]
   matrix[2] = matrixRotAU[8] * matrixRotZ[0] + matrixRotAU[9] * matrixRotZ[4]
-  // matrix[3] =
+  // matrix[3] = 0
   matrix[4] = matrixRotAU[0] * matrixRotZ[1] + matrixRotAU[1] * matrixRotZ[5]
   matrix[5] = matrixRotAU[4] * matrixRotZ[1] + matrixRotAU[5] * matrixRotZ[5]
   matrix[6] = matrixRotAU[8] * matrixRotZ[1] + matrixRotAU[9] * matrixRotZ[5]
-  // matrix[7] =
+  // matrix[7] = 0
   matrix[8] = matrixRotAU[2]
   matrix[9] = matrixRotAU[6]
   matrix[10] = matrixRotAU[10]
@@ -62,17 +62,7 @@ const aimSpin = function aimSpin ({theta, phi, spin}) {
   // matrix[13] = 0
   // matrix[14] = 0
   // matrix[15] = 1
-  /*
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-      matrix[4 * i + j] =// transpose cause glsl is column major
-        matrixRotZ[4 * i + 0] * matrixRotAU[4 * 0 + j] +
-        matrixRotZ[4 * i + 1] * matrixRotAU[4 * 1 + j] +
-        matrixRotZ[4 * i + 2] * matrixRotAU[4 * 2 + j] +
-        matrixRotZ[4 * i + 3] * matrixRotAU[4 * 3 + j]
-    }
-  }
-  */
+
   return matrix
 }
 
