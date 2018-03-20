579bfd5d5227f737b3215c9b49c998dd85df44c9
degumes <degumes@users.noreply.github.com>
2018-03-19 22:59:00 -0400

    chocolate hand

--- vertexCoordinates.js
+++ vertexCoordinates.js
@@ -1,14 +1,19 @@
 const scl = 6.0 // scale factor
 
 const vertexCoordinates = new Float32Array([
-  1.0 / scl, 1.0 / scl, 1.0 / scl, 1,
-  1.0 / scl, -1.0 / scl, 1.0 / scl, 1,
-  -1.0 / scl, -1.0 / scl, 1.0 / scl, 1,
-  -1.0 / scl, 1.0 / scl, 1.0 / scl, 1,
-  1.0 / scl, 1.0 / scl, -1.0 / scl, 1,
-  1.0 / scl, -1.0 / scl, -1.0 / scl, 1,
-  -1.0 / scl, -1.0 / scl, -1.0 / scl, 1,
-  -1.0 / scl, 1.0 / scl, -1.0 / scl, 1
+  0.375 / scl, 0.75 / scl, 0.375 / scl, 1,
+  0.375 / scl, -0.75 / scl, 0.375 / scl, 1,
+  -0.375 / scl, -0.75 / scl, 0.375 / scl, 1,
+  -0.375 / scl, 0.75 / scl, 0.375 / scl, 1,
+  0.5 / scl, 1.0 / scl, -0.5 / scl, 1,
+  0.5 / scl, -1.0 / scl, -0.5 / scl, 1,
+  -0.5 / scl, -1.0 / scl, -0.5 / scl, 1,
+  -0.5 / scl, 1.0 / scl, -0.5 / scl, 1
 ])
 
 export default vertexCoordinates
+
+/*
+3 0
+2 1
+ */
