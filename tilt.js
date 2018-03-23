05a07d3516b1de06cb6317def2f428c39e87d669
degumes <degumes@users.noreply.github.com>
2018-03-23 12:29:29 -0400

    thumbs OK

--- tilt.js
+++ tilt.js
@@ -13,8 +13,6 @@ const mkThumbEtaGammaUpdater = idx => ({eta, gamma}) => {
   const cg = Math.cos(gamma)
   const sg = Math.sin(gamma)
 
-  const q = -Math.SQRT1_2
-
   const a = []
   a[0] = ce
   a[1] = se
@@ -25,28 +23,53 @@ const mkThumbEtaGammaUpdater = idx => ({eta, gamma}) => {
   a[8] = se * sg
   a[9] = -ce * sg
   a[10] = cg
+  /*
+  // E * G
+  a[0] = ce
+  a[1] = se
+  a[2] = 0
+  a[4] = -se * cg
+  a[5] = ce * cg
+  a[6] = sg
+  a[8] = se * sg
+  a[9] = -ce * sg
+  a[10] = cg
 
-  // -rotY
+  // G * E
+  a[0] = ce
+  a[1] = cg * se
+  a[2] = sg * se
+  a[4] = -se
+  a[5] = cg * ce
+  a[6] = sg * ce
+  a[8] = 0
+  a[9] = -sg
+  a[10] = cg
+   */
+
+  const cPI4 = Math.cos(-Math.cos(Math.PI / 4))
+  const sPI4 = Math.sin(-Math.cos(Math.PI / 4))
+  // rotY * E * G
   const b = []
-  b[0] = q * (a[0] + a[2])
+  b[0] = cPI4 * a[0] + sPI4 * a[2]
   b[1] = a[1]
-  b[2] = q * (a[2] - a[0])
-  b[4] = q * (a[4] + a[6])
+  b[2] = -sPI4 * a[0] + cPI4 * a[2]
+  b[4] = cPI4 * a[4] + sPI4 * a[6]
   b[5] = a[5]
-  b[6] = q * (a[6] - a[4])
-  b[8] = q * (a[8] + a[10])
+  b[6] = -sPI4 * a[4] + cPI4 * a[6]
+  b[8] = cPI4 * a[8] + sPI4 * a[10]
   b[9] = a[9]
-  b[10] = q * (a[10] - a[8])
+  b[10] = -sPI4 * a[8] + cPI4 * a[10]
 
-  // -rotZ
-  matrixes[idx][0] = b[0]
-  matrixes[idx][1] = b[1]
+  // rotZ * rotY * E * G
+  matrixes[idx][0] = cPI4 * b[0] - sPI4 * b[1]
+  matrixes[idx][1] = sPI4 * b[0] + cPI4 * b[1]
   matrixes[idx][2] = b[2]
-  matrixes[idx][4] = b[4]
-  matrixes[idx][5] = b[5]
+  matrixes[idx][4] = cPI4 * b[4] - sPI4 * b[5]
+  matrixes[idx][5] = sPI4 * b[4] + cPI4 * b[5]
   matrixes[idx][6] = b[6]
-  matrixes[idx][8] = b[8]
-  matrixes[idx][9] = b[9]
+  matrixes[idx][8] = cPI4 * b[8] - sPI4 * b[9]
+  matrixes[idx][9] = sPI4 * b[8] + cPI4 * b[9]
   matrixes[idx][10] = b[10]
 }
 
@@ -120,6 +143,18 @@ matrixes[idx][6] = sg * ce
 matrixes[idx][9] = -sg
 matrixes[idx][10] = cg
 
+// rotY
+const b = []
+b[0] = q * (a[0] + a[2])
+b[1] = a[1]
+b[2] = q * (a[2] - a[0])
+b[4] = q * (a[4] + a[6])
+b[5] = a[5]
+b[6] = q * (a[6] - a[4])
+b[8] = q * (a[8] + a[10])
+b[9] = a[9]
+b[10] = q * (a[10] - a[8])
+
 // -rotZ
 matrixes[idx][0] = q * (b[0] - b[1])
 matrixes[idx][1] = q * (b[0] + b[1])
@@ -130,5 +165,16 @@ matrixes[idx][6] = b[6]
 matrixes[idx][8] = q * (b[8] - b[9])
 matrixes[idx][9] = q * (b[8] + b[9])
 matrixes[idx][10] = b[10]
+
+// rotZ * rotY * E * G
+matrixes[idx][0] = q * (b[0] - b[1])
+matrixes[idx][1] = q * (b[0] + b[1])
+matrixes[idx][2] = b[2] // não muda
+matrixes[idx][4] = q * (b[4] - b[5])
+matrixes[idx][5] = q * (b[4] + b[5])
+matrixes[idx][6] = b[6] // não muda
+matrixes[idx][8] = q * (b[8] - b[9])
+matrixes[idx][9] = q * (b[8] + b[9])
+matrixes[idx][10] = b[10] // não muda
 
 */
